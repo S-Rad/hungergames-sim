f254586ce77561f6929625b6e31cd9dc153eda61
S-Rad <42512223+S-Rad@users.noreply.github.com>
2020-04-10 21:18:01 +0200

    add token and test commands

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
+
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
+    client.on('ready', () => {
+        var generalChannel = client.channels.get("483264732518809690") // Replace with known channel ID
+        generalChannel.send("Hello, world!")  
+    })
 })
 
 // Get your bot's secret token from:
 // https://discordapp.com/developers/applications/
 // Click on your application -> Bot -> Token -> "Click to Reveal Token"
-bot_secret_token = "oRc5yDbqrcb0jgRLyPy4lRu4BC8pkDCE"
+bot_secret_token = "Njk4MjM2ODk4MTM2ODE3Nzc1.XpC7Kg.5SAm7o0_6_QEU0TbclDfl1Y64So"
+
+
 
 client.login(bot_secret_token)
